eca6341c3ed030c630ba872b0bff1da86e6d4877
vagrant <vagrant@homestead>
2018-09-10 16:21:39 +0000

    触发激活邮件

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+
+use App\Listeners\RegisteredListener;
+use Illuminate\Auth\Events\Registered;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
@@ -15,6 +18,9 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         'App\Events\Event' => [
             'App\Listeners\EventListener',
+        ],
+        Registered::class =>[
+            RegisteredListener::class,
         ],
     ];
 
