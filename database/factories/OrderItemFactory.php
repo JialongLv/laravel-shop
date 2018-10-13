335ece41da9e3ee9645eb3129a71bb37da220c8f
vagrant <vagrant@homestead>
2018-10-13 06:56:50 +0000

    完善假数据填充

---
+++ database/factories/OrderItemFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Models\OrderItem;
+use App\Models\Product;
+use Faker\Generator as Faker;
+
+$factory->define(OrderItem::class, function (Faker $faker) {
+
+    $product = Product::query()->where('on_sale', true)->isRandomOrder()->first();
+
+    $sku = $product->skus()->inRandomOrder()->first();
+    return [
+        'amount' => random_int(1,5),
+        'price'  => $sku->price,
+        'rating' => null,
+        'review' => null,
+        'reviewed_at' => null,
+        'product_id' => $product->id,
+        'product_sku_id' => $sku->id,
+    ];
+});
