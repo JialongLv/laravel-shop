8c0785b84dec937ad5c0c582c5919bf6c8b7d337
vagrant <vagrant@homestead>
2018-10-20 02:11:54 +0000

    后台众筹商品管理

--- app/Admin/Controllers/ProductsController.php
+++ app/Admin/Controllers/ProductsController.php
@@ -81,7 +81,7 @@ class ProductsController extends Controller
     {
         $grid = new Grid(new Product);
         // 使用 with 来预加载商品类目数据，减少 SQL 查询
-        $grid->model()->with(['category']);
+        $grid->model()->where('type',Product::TYPE_NORMAL)->with(['category']);
         $grid->id('ID')->sortable();
         $grid->title('商品名称');
         $grid->column('category.name', '类目');
@@ -142,6 +142,7 @@ class ProductsController extends Controller
 
         $form->text('title','商品名称')->rules('required');
         // 添加一个类目字段，与之前类目管理类似，使用 Ajax 的方式来搜索添加
+        $form->hidden('type')->value(Product::TYPE_NORMAL);
         $form->select('category_id', '类目')->options(function ($id) {
             $category = Category::find($id);
             if ($category) {
