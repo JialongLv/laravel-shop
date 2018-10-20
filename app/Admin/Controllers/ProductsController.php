4e6f267cc5394624d670decf9f1cac925e483fe4
vagrant <vagrant@homestead>
2018-10-20 02:47:19 +0000

    管理后台商品控制器封装

--- app/Admin/Controllers/ProductsController.php
+++ app/Admin/Controllers/ProductsController.php
@@ -11,163 +11,31 @@ use Encore\Admin\Layout\Content;
 use Encore\Admin\Show;
 use App\Models\Category;
 
-class ProductsController extends Controller
+class ProductsController extends CommonProductsController
 {
 
-    use HasResourceActions;
-
-    /**
-     * Index interface.
-     *
-     * @param Content $content
-     * @return Content
-     */
-    public function index(Content $content)
+    public function getProductType()
     {
-        return $content
-            ->header('商品列表')
-            ->description('description')
-            ->body($this->grid());
+        return Product::TYPE_NORMAL;
     }
 
-    /**
-     * Show interface.
-     *
-     * @param mixed   $id
-     * @param Content $content
-     * @return Content
-     */
-    public function show($id, Content $content)
+    protected function customGrid(Grid $grid)
     {
-        return $content
-            ->header('Detail')
-            ->description('description')
-            ->body($this->detail($id));
-    }
-
-    /**
-     * Edit interface.
-     *
-     * @param mixed   $id
-     * @param Content $content
-     * @return Content
-     */
-    public function edit($id, Content $content)
-    {
-        return $content
-            ->header('编辑商品')
-            ->body($this->form()->edit($id));
-    }
-
-    /**
-     * Create interface.
-     *
-     * @param Content $content
-     * @return Content
-     */
-    public function create(Content $content)
-    {
-        return $content
-            ->header('创建商品')
-            ->body($this->form());
-    }
-
-    /**
-     * Make a grid builder.
-     *
-     * @return Grid
-     */
-    protected function grid()
-    {
-        $grid = new Grid(new Product);
-        // 使用 with 来预加载商品类目数据，减少 SQL 查询
-        $grid->model()->where('type',Product::TYPE_NORMAL)->with(['category']);
+        $grid->model()->with(['category']);
         $grid->id('ID')->sortable();
         $grid->title('商品名称');
         $grid->column('category.name', '类目');
-        $grid->on_sale('已上架')->display(function ($value){
-           return $value ? '是' : '否';
+        $grid->on_sale('已上架')->display(function ($value) {
+            return $value ? '是' : '否';
         });
         $grid->price('价格');
         $grid->rating('评分');
         $grid->sold_count('销量');
-        $grid->review_count('评论量');
-
-        $grid->actions(function ($actions){
-            $actions->disableView();
-            $actions->disableDelete();
-        });
-
-        $grid->tools(function ($tools){
-            $tools->batch(function ($batch){
-               $batch->disableDelete();
-            });
-        });
-        return $grid;
+        $grid->review_count('评论数');
     }
 
-    /**
-     * Make a show builder.
-     *
-     * @param mixed   $id
-     * @return Show
-     */
-    protected function detail($id)
+    protected function customForm(Form $form)
     {
-        $show = new Show(Product::findOrFail($id));
-
-        $show->id('Id');
-        $show->title('Title');
-        $show->description('Description');
-        $show->image('Image');
-        $show->on_sale('On sale');
-        $show->rating('Rating');
-        $show->sold_count('Sold count');
-        $show->review_count('Review count');
-        $show->price('Price');
-        $show->created_at('Created at');
-        $show->updated_at('Updated at');
-
-        return $show;
-    }
-
-    /**
-     * Make a form builder.
-     *
-     * @return Form
-     */
-    protected function form()
-    {
-        $form = new Form(new Product);
-
-        $form->text('title','商品名称')->rules('required');
-        // 添加一个类目字段，与之前类目管理类似，使用 Ajax 的方式来搜索添加
-        $form->hidden('type')->value(Product::TYPE_NORMAL);
-        $form->select('category_id', '类目')->options(function ($id) {
-            $category = Category::find($id);
-            if ($category) {
-                return [$category->id => $category->full_name];
-            }
-        })->ajax('/admin/api/categories?is_directory=0');
-
-        $form->image('image', '封面图片')->rules('required|image');
-
-        $form->editor('description','商品描述')->rules('required');
-
-        $form->radio('on_sale','上架')->options(['1' => '是', '0'=> '否'])->default('0');
-
-        $form->hasMany('skus', 'SKU 列表', function (Form\NestedForm $form)
-        {
-           $form->text('title','SKU 名称')->rules('required');
-           $form->text('description', 'SKU 描述')->rules('required');
-           $form->text('price', '单价')->rules('required|numeric|min:0.01');
-           $form->text('stock', '剩余库存')->rules('required|integer|min:0');
-        });
-
-        $form->saving(function (Form $form){
-            $form->model()->price = collect($form->input('skus'))->where(Form::REMOVE_FLAG_NAME, 0)->min('price') ?: 0;
-        });
-
-        return $form;
+        // 普通商品没有额外的字段，因此这里不需要写任何代码
     }
 }
