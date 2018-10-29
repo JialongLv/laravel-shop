7c2fbe20c602a1829db2d52f6211d5b4699116b5
vagrant <vagrant@homestead>
2018-10-29 08:19:53 +0000

    修复分面搜索 Bug

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -115,8 +115,7 @@ class ProductsController extends Controller
                     'nested' => [
                       'path' => 'properties',
                       'query' => [
-                            ['term' => ['properties.name' => $name]],
-                            ['term' => ['properties.value' => $value]],
+                          ['term' => ['properties.search_value' => $filter]],
                       ],
                     ],
                 ];
