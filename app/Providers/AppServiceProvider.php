b68f3c8899cb6ace8182216d1f18bf328ab7756f
jialong <ljl9410@126.com>
2018-11-02 15:16:34 +0800

    减少秒杀接口 SQL 查询

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,6 +7,7 @@ use Monolog\Logger;
 use  Yansongda\Pay\Pay;
 use Carbon\Carbon;
 use Elasticsearch\ClientBuilder as ESClientBuilder;
+use Illuminate\Support\Str;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,11 @@ class AppServiceProvider extends ServiceProvider
     {
         \View::composer(['products.index', 'products.show'], \App\Http\ViewComposers\CategoryTreeComposer::class);
          Carbon::setLocale('zh');
+        if (app()->environment('local')) {
+            \DB::listen(function ($query) {
+                \Log::info(Str::replaceArray('?', $query->bindings, $query->sql));
+            });
+        }
     }
 
     /**
